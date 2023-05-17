705a6d7d3e07456a3f91138a3e6d0f8dabdd905a
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-05-17 04:39:57 +0000

    [add]2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def index
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,7 @@ module.exports = environment
 
 const webpack = require('webpack')
 environment.plugins.prepend(
-  'Provide'
+  'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
